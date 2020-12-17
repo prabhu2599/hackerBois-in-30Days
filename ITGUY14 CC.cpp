476460aaea85f95ceb5eed68a1b1d2f9c40742f4
terminatorPG <55758267+prabhu2599@users.noreply.github.com>
2020-12-17 18:37:54 +0530

    Create ITGUY14 CC.cpp

---
+++ ITGUY14 CC.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// void printVector(vector<int> v) {
+//     for(int i: v) {
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }
+
+int main() {
+    int n = 0;
+    cin>>n;
+    //vector<int> v;
+    
+    for(int i=0;i<n;i++) {
+        int x = 0;
+        cin>>x;
+        for(int j=0;j<x;j++) {
+            int x2 = 0;
+            cin>>x2;
+            if(j == x2) {
+                //v.push_back(x2);
+                cout<<x2<<" ";
+            } else {
+                //v.push_back(0);
+                cout<<0<<" ";
+            }
+    }
+    //printVector(v);
+    cout<<endl;
+    //v.clear();
+    }
+    return 0;
+}
